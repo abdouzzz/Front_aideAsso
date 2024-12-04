910c6294d0a99940433ddfd54dff060d5caad96a
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2024-12-04 14:14:15 +0100

    feat:ajout url membre et tresorerrie à l'api

--- src/constants/ApiUrls.ts
+++ src/constants/ApiUrls.ts
@@ -4,3 +4,5 @@ export const ApiUrl = config.VUE_APP_API_BASE_URL;
 
 export const ApiUrlUsers = `${ApiUrl}/user`;
 export const ApiUrlAssos = `${ApiUrl}/association`;
+export const ApiUrlMembres = `${ApiUrl}/membre`;
+export const ApiUrlTresories = `${ApiUrl}/tresorerie`;
