51d8a0c5b0b470bc15dff25f646576f3e7807eae
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2024-12-04 14:09:09 +0100

    feat:création page api membre

---
+++ src/composables/member/MemberAPI.ts
@@ -0,0 +1,19 @@
+import axiosInstance from '@/config/AxiosConfig';
+import { ApiUrlMembres, ApiUrlAssos } from '@/constants/ApiUrls';
+
+export function useMemberApi() {
+  return {
+    async updateMember(id: number, updatedMember: any) {
+      const res = await axiosInstance.put(`${ApiUrlMembres}/update/${id}`, updatedMember);
+      return res.data.message;
+    },
+    async addMember(id: number, newMember: any){
+      const res = await axiosInstance.post(`${ApiUrlAssos}/${id}/add/membres`, newMember);
+      return res.data.message;
+    },
+    async deleteMember(id: number){
+      const res = await axiosInstance.delete(`${ApiUrlMembres}/delete/${id}`);
+      return res.data.message;
+    },
+  };
+}
